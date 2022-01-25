a6e5ba9dd27bf26bb5209381d18d9037fef555af
mixvert <bajkowski1e@gmail.com>
2022-01-25 17:33:17 +0100

    Aktualizacja plików

--- navbar.php
+++
@@ -1,9 +0,0 @@
-<nav class="navbar navbar-dark py-3" style="background: #fd7e14;">
-    <a class="navbar-brand text-center" href="index.php" id="navbar">
-        <h3 class="mb-0">FoodDeli</h3>
-    </a>
-    <form class="form-inline" id="navbar-button">
-        <button class="btn btn-outline-light mr-1" type="button" onclick="location.href='index_login.php'">Zaloguj się</button>
-        <button class="btn btn-light" type="button" onclick="location.href='index_register_user.php'">Zarejestruj się</button>
-    </form>
-</nav>
