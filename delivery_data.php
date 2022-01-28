3d11e13f58685445316d98dfb960922f6c1cf041
Igor Boradyn <boradyn555@gmail.com>
2022-01-28 01:47:40 +0100

    wczytywanie danych z BD i podsumowanie zamówienia

--- delivery_data.php
+++ delivery_data.php
@@ -1,3 +1,19 @@
+<?php
+	session_start();
+	if(!(isset($_SESSION['logged']) && $_SESSION['logged']))
+		header("Location: index.php");
+	require_once "connect.php";
+
+	$connect = @new mysqli($host, $db_user, $db_password, $db_name);
+
+	$connect->set_charset('utf8');
+
+	if($connect->connect_errno!=0){
+		echo "Error: ".$connect->connect_errno;
+	}
+	else{
+		$restaurantID = $_GET['id'];
+?>
 <!DOCTYPE html>
 <html lang='pl'>
 <head>
@@ -14,13 +30,19 @@
 
 <header></header>
 
-<h2 class='fluid bg-light text-center mb-0 pt-4 pb-3'>Cudowne Manowce Restaurant</h2>
+<?php
+	$userID = $_SESSION['userID'];
+	$restaurantID = $_GET['id'];
+	$restaurant = $connect->query("SELECT * FROM restaurants WHERE ID='$restaurantID'")->fetch_assoc();
+?>
+
+<h2 class='fluid bg-light text-center mb-0 pt-4 pb-3'><?php echo $restaurant['name']; ?></h2>
 
 <div class="container">
   	<div class="row">
     	<div class="col-2 text-center">
       		<div class="bg-light text-center" style="height: 0px;">
-				<button class="btn btn-light border" id='back' type="button" onclick="location.href='shopping_cart.php'" style="margin-top: -95px; width: 90px" >Powrót</button>
+				<button class="btn btn-light border" id='back' type="button" onclick="location.href='shopping_cart.php?id=<?php echo $restaurantID; ?>'" style="margin-top: -95px; width: 90px" >Powrót</button>
 			</div>
     	</div>
   	</div>
@@ -40,43 +62,43 @@
   						<div class="form-row">
   							<div class="col-10 pb-1">
       							<label for="validationCustom07">Imię i nazwisko</label>
-      							<input type="text" class="form-control" id="validationCustom07" placeholder="Wpisz swoje imię i nazwisko" required>
+      							<input type="text" class="form-control" id="validationCustom07" placeholder="Wpisz swoje imię i nazwisko" value="<?php echo $_SESSION['firstName']." ".$_SESSION['lastName'] ?>" required>
       							<div class="invalid-feedback">Należy podać imię i nazwisko</div>
     						</div>
     						<div class="w-100"></div>
     						<div class="col-10 pb-1 pt-1">
       							<label for="validationCustom01">Ulica</label>
-      							<input type="text" class="form-control" id="validationCustom01" placeholder="Wpisz nazwę ulicy" required>
+      							<input type="text" class="form-control" id="validationCustom01" placeholder="Wpisz nazwę ulicy" value="<?php echo $_SESSION['street'] ?>" required>
       							<div class="invalid-feedback">Nazwa ulicy jest wymagana</div>
     						</div>
     						<div class="w-100"></div>
 						    <div class="col-10 pb-1 pt-1">
 							    <label for="validationCustom02">Numer budynku</label>
-							    <input type="text" class="form-control" id="validationCustom02" placeholder="Wpisz numer budynku" required>
+							    <input type="text" class="form-control" id="validationCustom02" placeholder="Wpisz numer budynku" value="<?php echo $_SESSION['streetNumber'] ?>" required>
 							    <div class="valid-feedback">Looks good!</div>
 								<div class="invalid-feedback">Numer domu jest wymagany</div>
 						    </div>
    							<div class="w-100"></div>
 						    <div class="col-10 pb-1 pt-1">
 							    <label for="validationCustom03">Numer mieszkania</label>
-							    <input type="text" class="form-control" id="validationCustom03" placeholder="Wpisz numer mieszkania" required>
+							    <input type="text" class="form-control" id="validationCustom03" placeholder="Wpisz numer mieszkania" value="<?php echo $_SESSION['flatNumber'] ?>" required>
 								<div class="invalid-feedback">Numer mieszkania jest wymagany</div>
 						    </div>
 							<div class="w-100"></div>
 						    <div class="col-10 pb-1 pt-1">
 						      	<label for="validationCustom04">Numer telefonu</label>
-						      	<input type="tel" class="form-control" id="validationCustom04" placeholder="Wpisz swój numer telefonu" pattern="[0-9]{9}" required>
+						      	<input type="tel" class="form-control" id="validationCustom04" placeholder="Wpisz swój numer telefonu" pattern="[0-9]{9}" value="<?php echo $_SESSION['phone'] ?>" required>
 						      	<div class="invalid-feedback">Należy podać numer telefonu</div>
 						    </div>							
 						    <div class="w-100"></div>
 						    <div class="col-3 pb-1 pt-1">
       							<label for="validationCustom05">Kod pocztowy</label>
-								<input type="text" class="form-control" id="validationCustom05" placeholder="Wpisz swój kod pocztowy" pattern="[0-9]{2}[-][0-9]{3}" required>
+								<input type="text" class="form-control" id="validationCustom05" placeholder="Wpisz swój kod pocztowy" pattern="[0-9]{2}[-][0-9]{3}" value="<?php echo $_SESSION['postcode'] ?>" required>
       							<div class="invalid-feedback">Należy podać kod pocztowy</div>
     						</div>
 						    <div class="col-7 pb-1 pt-1">
 						      	<label for="validationCustom06">Miasto</label>
-						      	<input type="text" class="form-control" id="validationCustom06" placeholder="Wpisz swoje miasto" required>
+						      	<input type="text" class="form-control" id="validationCustom06" placeholder="Wpisz swoje miasto" value="<?php echo $_SESSION['city'] ?>" required>
 						      	<div class="invalid-feedback">Należy podać miasto</div>
 						    </div>
   						</div>
@@ -131,9 +153,22 @@
 				</div>
 				<div class="w-100"></div>
 				<div class="col pl-5">
-					<div>Produkty: 156zł</div>
-					<div>Koszt dostawy: 10zł</div>
-					<div>Do zapłaty: 166zł</div>
+					<?php
+						$rows = $connect->query("SELECT ID_product, amount FROM products_list INNER JOIN shopping_cart ON shopping_cart.ID_products_list = products_list.ID WHERE ID_client = '$userID' AND shopping_cart.ID_restaurant = '$restaurantID';");
+						$num_rows = $rows->num_rows;
+						$sum = 0;
+						for($i = 0; $i < $num_rows; $i++){
+							$position = $rows->fetch_assoc();
+							$productID = $position['ID_product'];
+							$amount = $position['amount'];
+				
+							$product = $connect->query("SELECT price FROM products WHERE ID = '$productID'")->fetch_assoc();
+							$sum += $product['price']*$amount;
+						}
+					?>
+					<div>Produkty: <?php echo number_format($sum, 2); ?> zł</div>
+					<div>Koszt dostawy: <?php echo number_format($restaurant['delivery_cost'], 2); ?> zł</div>
+					<div>Do zapłaty: <?php echo number_format($sum+$restaurant['delivery_cost'], 2); ?> zł</div>
 				</div>
 			</div>
 		</div>
@@ -151,12 +186,7 @@
 
 <script>
 	$("header").load("navbar.php");
-</script>
 
-</body>
-</html>
-
-<script>
 	(function() {
 		'use strict';
 	  	window.addEventListener('load', function() {
@@ -172,4 +202,9 @@
 		    });
 	  	}, false);
 	})();
+
 </script>
+
+</body>
+</html>
+<?php } ?>
