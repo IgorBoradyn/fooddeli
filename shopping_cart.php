b2a4f2adfd6583a9b0460f4da0629fa5983a9372
Igor Boradyn <boradyn555@gmail.com>
2022-01-28 01:19:46 +0100

    dodanie działającego wypisywania i usuwania produktów z koszyka

--- shopping_cart.php
+++ shopping_cart.php
@@ -30,7 +30,13 @@
 
 <header></header>
 
-<h2 class='fluid bg-light text-center mb-0 pt-4 pb-3'>Cudowne Manowce Restaurant - Koszyk</h2>
+<?php
+	$userID = $_SESSION['userID'];
+	$restaurantID = $_GET['id'];
+	$restaurant = $connect->query("SELECT * FROM restaurants WHERE ID='$restaurantID'")->fetch_assoc();
+?>
+
+<h2 class='fluid bg-light text-center mb-0 pt-4 pb-3'><?php echo $restaurant['name']; ?></h2>
 
 <div class="container">
   	<div class="row">
@@ -43,7 +49,7 @@
 </div>
 
 
-<div class="bg-light text-center pb-3">
+<div class="bg-light text-center">
 	<div class="btn-group" role="group" aria-label="Basic example" style="margin-left: auto; margin-right:auto;">
   		<button type="button" class="btn border bordercolor resto shadow-none" data-toggle="modal" data-target="#exampleModal" style="background: rgb(234, 236, 239);">O restauracji</button>
 		<div class="modal fade rounded" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -61,7 +67,7 @@
 						 		<div class="col border border-bottom-0 pb-1 pl-2 pt-1 rounded-top text-left lead" style="font-size: 16px; background: #fd7e14; color: white;">Lokalizacja</div>
     							<div class="w-100"></div>
 						    	<div class="col p-0">
-						    		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9431.199034800073!2d20.4768723!3d53.775252!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x723fbb647fb17c03!2sCudne%20Manowce%20Restauracja!5e0!3m2!1spl!2spl!4v1642758316583!5m2!1spl!2spl" class="border" width="100%" height="350px" allowfullscreen="" loading="lazy"></iframe>
+						    		<?php echo $restaurant['location']; ?>
 						    	</div>
 						 	</div>
 						</div>
@@ -77,26 +83,27 @@
 						    	<div class="col p-0">
 						    		<div class="container">
 							    		<div class="row">
+											<?php $open_hours = json_decode($restaurant['open_hours'], true);?>
 											<div class="col text-left pl-4 lead" style="font-size: 16px;">Poniedziałek</div>
-										  	<div class="col text-right pr-4 lead" style="font-size: 16px;">11:00 - 22:30</div>
+										  	<div class="col text-right pr-4 lead" style="font-size: 16px;"><?php echo $open_hours["Monday"]["open"]." - ".$open_hours["Monday"]["close"]; ?></div>
 										  	<div class="w-100"></div>
 										  	<div class="col text-left pl-4 lead" style="font-size: 16px;">Wtorek</div>
-										  	<div class="col text-right pr-4 lead" style="font-size: 16px;">11:00 - 22:30</div>
+										  	<div class="col text-right pr-4 lead" style="font-size: 16px;"><?php echo $open_hours["Thuesday"]["open"]." - ".$open_hours["Thuesday"]["close"]; ?></div>
 										  	<div class="w-100"></div>
 										  	<div class="col text-left pl-4 lead" style="font-size: 16px;">Środa</div>
-										  	<div class="col text-right pr-4 lead" style="font-size: 16px;">11:00 - 22:30</div>
+										  	<div class="col text-right pr-4 lead" style="font-size: 16px;"><?php echo $open_hours["Wednesday"]["open"]." - ".$open_hours["Wednesday"]["close"]; ?></div>
 										  	<div class="w-100"></div>
 										  	<div class="col text-left pl-4 lead" style="font-size: 16px;">Czwartek</div>
-										  	<div class="col text-right pr-4 lead" style="font-size: 16px;">11:00 - 22:30</div>
+										  	<div class="col text-right pr-4 lead" style="font-size: 16px;"><?php echo $open_hours["Thursday"]["open"]." - ".$open_hours["Thursday"]["close"]; ?></div>
 										  	<div class="w-100"></div>
 										  	<div class="col text-left pl-4 lead" style="font-size: 16px;">Piątek</div>
-										  	<div class="col text-right pr-4 lead" style="font-size: 16px;">11:00 - 22:30</div>
+										  	<div class="col text-right pr-4 lead" style="font-size: 16px;"><?php echo $open_hours["Friday"]["open"]." - ".$open_hours["Friday"]["close"]; ?></div>
 										  	<div class="w-100"></div>
 										  	<div class="col text-left pl-4 lead" style="font-size: 16px;">Sobota</div>
-										  	<div class="col text-right pr-4 lead" style="font-size: 16px;">11:00 - 22:30</div>
+										  	<div class="col text-right pr-4 lead" style="font-size: 16px;"><?php echo $open_hours["Saturday"]["open"]." - ".$open_hours["Saturday"]["close"]; ?></div>
 										  	<div class="w-100"></div>
 										  	<div class="col text-left pl-4 lead" style="font-size: 16px;">Niedziela</div>
-										  	<div class="col text-right pr-4 lead" style="font-size: 16px;">11:00 - 22:30</div>
+										  	<div class="col text-right pr-4 lead" style="font-size: 16px;"><?php echo $open_hours["Sunday"]["open"]." - ".$open_hours["Sunday"]["close"]; ?></div>
 										</div>
 									</div>
 						    	</div>
@@ -115,10 +122,10 @@
 						    		<div class="container">
 							    		<div class="row">
 							    			<div class="col text-left pl-4 lead text-nowrap" style="font-size: 16px;">Minimalna kwota zamówienia</div>
-										  	<div class="col text-right pr-4 lead" style="font-size: 16px;">Brak</div>
+										  	<div class="col text-right pr-4 lead" style="font-size: 16px;"><?php echo number_format($restaurant['min_delivery_value'], 2)." zł"; ?></div>
 										  	<div class="w-100"></div>
 											<div class="col text-left pl-4 lead" style="font-size: 16px;">Koszty dostawy</div>
-										  	<div class="col text-right pr-4 lead" style="font-size: 16px;">10 zł</div>
+										  	<div class="col text-right pr-4 lead" style="font-size: 16px;"><?php echo number_format($restaurant['delivery_cost'], 2)." zł"; ?></div>
 										</div>
 									</div>
 						    	</div>
@@ -136,11 +143,11 @@
 						    	<div class="col p-0">
 						    		<div class="container">
 							    		<div class="row">
-							    			<div class="col text-left pl-4 lead text-nowrap" style="font-size: 16px;">Cudowne Manowce Restaurant</div>
+							    			<div class="col text-left pl-4 lead text-nowrap" style="font-size: 16px;"><?php echo $restaurant['name']; ?></div>
 										  	<div class="w-100"></div>
-											<div class="col text-left pl-4 lead" style="font-size: 16px;">Bolesława Chrobrego 4A</div>
+											<div class="col text-left pl-4 lead" style="font-size: 16px;"><?php echo $restaurant['adress']; ?></div>
 										  	<div class="w-100"></div>
-											<div class="col text-left pl-4 lead" style="font-size: 16px;">10-033 Olsztyn</div>
+											<div class="col text-left pl-4 lead" style="font-size: 16px;"><?php echo $restaurant['postcode']." ".$restaurant['city']; ?></div>
 										</div>
 									</div>
 						    	</div>
@@ -164,10 +171,10 @@
       					<div class="container">
 							<div class="row">
 							    <div class="col text-left pl-4 lead text-nowrap" style="font-size: 16px;">Adres email</div>
-							    <a class="col text-right pr-4 lead" style="font-size: 16px; color: #212529;" href="mailto:sample@mail.com">sample@mail.com</a>
+							    <a class="col text-right pr-4 lead" style="font-size: 16px; color: #212529;" href="mailto:<?php echo $restaurant['email']; ?>"><?php echo $restaurant['email']; ?></a>
 								<div class="w-100"></div>
 								<div class="col text-left pl-4 lead" style="font-size: 16px;">Numer telefonu</div>
-								<a class="col text-right pr-4 lead" style="font-size: 16px; color: #212529;" href="tel:+48123456789">+123456789</a>
+								<a class="col text-right pr-4 lead" style="font-size: 16px; color: #212529;" href="tel:<?php echo $restaurant['phone']; ?>"><?php echo $restaurant['phone']; ?></a>
 							</div>
 						</div>
       				</div>
@@ -200,130 +207,55 @@
 </div>
 
 <div class="container-fluid bg-light">
-	<div class="row" style="width: 85%; margin-left: auto; margin-right: auto;">
-  		<div class="resto col border bordercolor" style="background: rgb(234, 236, 239);">
-  			<a href="ndex.php" style="color: black; text-decoration: none;">
+	<div class="row" style="width: 85%; margin-left: auto; margin-right: auto; padding-top: 16px;">
+	<?php
+
+		$rows = $connect->query("SELECT * FROM products_list INNER JOIN shopping_cart ON shopping_cart.ID_products_list = products_list.ID WHERE ID_client = '$userID' AND shopping_cart.ID_restaurant = '$restaurantID';");
+		$num_rows = $rows->num_rows;
+		for($i = 0; $i < $num_rows; $i++){
+			$position = $rows->fetch_assoc();
+			$productID = $position['ID_product'];
+
+//################## Tą zmienną trzeba dodać do znacznika, kótry będzie pokazywał ilość produktów ####################################
+			$amount = $position['amount'];
+//####################################################################################################################################
+
+			$product = $connect->query("SELECT * FROM products WHERE ID = '$productID'")->fetch_assoc();
+			$name = $product['name'];
+			$desc = $product['description'];
+			$img = $product['photo'];
+			$price = number_format($product['price'], 2);
+echo<<<EOT
+		<div class="resto col border bordercolor" style="background: rgb(234, 236, 239);">
+			<a href="shopping_cart.php?id={$restaurantID}#{$productID}" class="product-remove" style="color: black; text-decoration: none;">
 				<div class="row">
 					<div class="col-2 p-0 border bordercolor">
-						<img src="wall.JPEG" class="img-fluid" alt="Responsive image">
+						<img src="{$img}" class="img-fluid" alt="Responsive image">
 					</div>
 					<div class="col-10">
 						<div class="row" style="min-height: 100%;">
-		  					<div class="col h-50 lead" style="padding-top: 15px; padding-left: 7%; font-size: 22px;">Kubełek 30 Hot Wings</div>
-		  					<div class="w-100"></div>
-		  					<div class="col h-50 lead" style="padding-left: 10%; font-size: 16px;">30 Hot Wings (pikantne skrzydełka), 2x frytki 40g</div>
-		  					<div class="w-100"></div>
-		  					<div class="col text-right lead" style="padding-bottom: 15px; padding-right: 10%; font-size: 22px;">
-		 						<i class="fas fa-money-bill-wave-alt" style="color: #fd7e14"></i>
-		 							39,00 zł
-		 					</div>
+								<div class="col h-50 lead" style="padding-top: 15px; padding-left: 7%; font-size: 22px;">{$name}</div>
+								<div class="w-100"></div>
+								<div class="col h-50 lead" style="padding-left: 10%; font-size: 16px;">{$desc}</div>
+								<div class="w-100"></div>
+								<div class="col text-right lead" style="padding-bottom: 15px; padding-right: 10%; font-size: 22px;">
+								<i class="fas fa-money-bill-wave-alt" style="color: #fd7e14"></i>
+									{$price} zł
+							</div>
 						</div>
 					</div>
 				</div>
 			</a>
-  		</div>
-  		<div class="col-1" style="display: flex; justify-content: center; align-items: center;">
+		</div>
+		<div class="col-1" style="display: flex; justify-content: center; align-items: center;">
 			<button id="btn_plus" class="btn ml-2">
 				<i class="fas fa-plus"></i>
 			</button>
-  		</div>
-  		<div class="w-100" style="padding-top: 30px"></div>
-  		<div class="resto col border bordercolor" style="background: rgb(234, 236, 239);">
-  			<a href="index.php" style="color: black; text-decoration: none;">
-				<div class="row">
-					<div class="col-2 p-0 border bordercolor">
-						<img src="wall.JPEG" class="img-fluid" alt="Responsive image">
-					</div>
-					<div class="col-10">
-						<div class="row" style="min-height: 100%;">
-		  					<div class="col h-50 lead" style="padding-top: 15px; padding-left: 7%; font-size: 22px;">Kubełek 30 Hot Wings</div>
-		  					<div class="w-100"></div>
-		  					<div class="col h-50 lead" style="padding-left: 10%; font-size: 16px;">30 Hot Wings (pikantne skrzydełka), 2x frytki 40g</div>
-		  					<div class="w-100"></div>
-		  					<div class="col text-right lead" style="padding-bottom: 15px; padding-right: 10%; font-size: 22px;">
-		  						<i class="fas fa-star" style="color: #fd7e14"></i> 
-		  							4.5 (30)&emsp;
-		 						<i class="fas fa-money-bill-wave-alt" style="color: #fd7e14"></i>
-		 							39,00 zł
-		 					</div>
-						</div>
-					</div>
-				</div>
-			</a>
-  		</div>
-  		<div class="col-1" style="display: flex; justify-content: center; align-items: center;">
-			<button id="btn_plus" class="btn ml-2">
-				<i class="fas fa-plus"></i>
-			</button>
-  		</div>
-  		<div class="w-100" style="padding-top: 30px"></div>
-  		<div class="resto col border bordercolor" style="background: rgb(234, 236, 239);">
-  			<a href="index.php" style="color: black; text-decoration: none;">
-				<div class="row">
-					<div class="col-2 p-0 border bordercolor">
-						<img src="wall.JPEG" class="img-fluid" alt="Responsive image">
-					</div>
-					<div class="col-10">
-						<div class="row" style="min-height: 100%;">
-		  					<div class="col h-50 lead" style="padding-top: 15px; padding-left: 7%; font-size: 22px;">Kubełek 30 Hot Wings</div>
-		  					<div class="w-100"></div>
-		  					<div class="col h-50 lead" style="padding-left: 10%; font-size: 16px;">30 Hot Wings (pikantne skrzydełka), 2x frytki 40g</div>
-		  					<div class="w-100"></div>
-		  					<div class="col text-right lead" style="padding-bottom: 15px; padding-right: 10%; font-size: 22px;">
-		  						<i class="fas fa-star" style="color: #fd7e14"></i> 
-		  							4.5 (30)&emsp;
-		 						<i class="fas fa-money-bill-wave-alt" style="color: #fd7e14"></i>
-		 							39,00 zł
-		 					</div>
-						</div>
-					</div>
-				</div>
-			</a>
-  		</div>
-  		<div class="col-1" style="display: flex; justify-content: center; align-items: center;">
-			<button id="btn_plus" class="btn ml-2">
-				<i class="fas fa-plus"></i>
-			</button>
-  		</div>
-  		<div class="w-100" style="padding-top: 30px"></div>
-  		<div class="resto col border bordercolor" style="background: rgb(234, 236, 239);">
-  			<a href="index.php" style="color: black;     text-decoration: none;">
-				<div class="row">
-					<div class="col-2 p-0 border bordercolor">
-						<img src="wall.JPEG" class="img-fluid" alt="Responsive image">
-					</div>
-					<div class="col-10">
-						<div class="row" style="min-height: 100%;">
-		  					<div class="col h-50 lead" style="padding-top: 15px; padding-left: 7%; font-size: 22px;">Kubełek 30 Hot Wings</div>
-		  					<div class="w-100"></div>
-		  					<div class="col h-50 lead" style="padding-left: 10%; font-size: 16px;">30 Hot Wings (pikantne skrzydełka), 2x frytki 40g</div>
-		  					<div class="w-100"></div>
-		  					<div class="col text-right lead" style="padding-bottom: 15px; padding-right: 10%; font-size: 22px;">
-		  						<i class="fas fa-star" style="color: #fd7e14"></i> 
-		  							4.5 (30)&emsp;
-		 						<i class="fas fa-money-bill-wave-alt" style="color: #fd7e14"></i>
-		 							39,00 zł
-		 					</div>
-						</div>
-					</div>
-				</div>
-			</a>
-  		</div>
-  		<div class="col-1" style="display: flex; justify-content: center; align-items: center;">
-			<button id="btn_plus" class="btn ml-2">
-				<i class="fas fa-plus"></i>
-			</button>
-  		</div>
-	</div>
-	<h2 class='fluid bg-light text-right mb-0 pt-4 pb-1' style="margin-right: 10%">Do zapłaty: 156zł</h2>
-	<div class="container">
-  		<div class="row">
-    		<div class="col text-center pt-2 pb-4">
-      			<div class="bg-light text-center" style="height: 0px;">
-					<button class="btn border bordercolor resto shadow-none btn-lg" id='back' type="button" onclick="location.href='delivery_data.php'" style="background: rgb(234, 236, 239);">Zamów</button>
-				</div>
-    		</div>
-  		</div>
+		</div>
+		<div class="w-100" style="padding-top: 30px"></div>
+EOT;
+		}
+	?>
 	</div>
 </div>
 
@@ -335,6 +267,22 @@
 
 <script>
 	$("header").load("navbar.php");
+
+	$(".product-remove").click(function(event){
+		event.preventDefault();
+
+		var href = this.href;
+		var productID = href.split('#').pop();
+
+		$.post('remove_product_from_cart.php',{
+			restID: <?php echo $restaurantID ?>,
+			productID: productID
+		}, function(){
+			window.location = href;
+			location.reload();
+		});
+
+	});
 </script>
 
 </body>
